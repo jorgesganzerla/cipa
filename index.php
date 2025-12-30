7439bc4df3f8910fdae3984bd279ebc06359fddf
jorgesganzerla <153621584+jorgesganzerla@users.noreply.github.com>
2025-12-30 09:20:18 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Exibir Imagem</title>
+    </head>
+    <body>
+        <?php
+            // Caminho para a imagem no servidor
+            $caminho_imagem = "d622603007dd2a356b00065841fc4136h.png"; 
+        ?>
+        
+        <img src="<?php echo $caminho_imagem; ?>" alt="Descrição da minha imagem" width="500">
+    </body>
+    </html>
